fd32c189eb6d3dab4a6971bbfb337fa2b3367647
Artem Shvorin <art@shvorin.net>
2019-08-06 19:54:03 +0200

    trying to use a new "database" format

--- tools/merge.py
+++ tools/merge.py
@@ -8,6 +8,82 @@ from grammar import *
 
 dirname = os.path.dirname(sys.argv[0])
 
+class Raw_FTable(object):
+    def __init__(self, fname):
+        t = {}
+        nCols_min = 0
+        nCols_max = 0
+        row = 0
+        with open(fname) as f:
+            lines = f.readlines()
+            self.nRows = len(lines)
+            for line in lines:
+                line = line.rstrip() # remove trailing '\n'
+                col = 0
+                items = string.split(line, '\t')
+                for item in items:
+                    t[(row, col)] = item
+                    col += 1
+                row += 1
+                nItems = len(items)
+                if nCols_min == 0 or nItems < nCols_min:
+                    nCols_min = nItems
+                if nItems > nCols_max:
+                    nCols_max = nItems
+        self.raw_table = t
+        self.nCols = nCols_max
+        self.straight = nCols_min == nCols_max
+
+    def __get__(self, idx):
+        return get(self.raw_table, idx)
+
+class Noun_FTable(Raw_FTable):
+    """
+singulár | plurál
+"""
+    number_list = GrammarNumber.singular, GrammarNumber.plural
+
+    def __init__(self, fname):
+        super(Noun_FTable, self).__init__(fname)
+        t = {}
+        for case in GrammarCase.__members__.itervalues():
+            for number_idx, number in enumerate(Noun_FTable.number_list):
+                row, col = case.value - 1, number_idx
+                try:
+                    t[(case, number)] = self.raw_table[(row, col)]
+                except KeyError:
+                    pass
+        self.noun_table = t
+
+    def get(self, case, number, gender):
+        return self.noun_table[(case, number)]
+
+class Adjective_FTable(Raw_FTable):
+    """
+Číslo |	           singulár           |             plurál
+Rod   | m. živ. | m. neživ. | ž. | s. | m. živ. | m. neživ. | ž. | s.
+
+This format is also suitable for possessive pronouns.
+"""
+    number_list = GrammarNumber.singular, GrammarNumber.plural
+
+    def __init__(self, fname):
+        super(Adjective_FTable, self).__init__(fname)
+        t = {}
+        for case in GrammarCase.__members__.itervalues():
+            for number_idx, number in enumerate(Adjective_FTable.number_list):
+                for gender_idx, gender in enumerate(GrammarGender.__members__.values()):
+                    row = case.value - 1
+                    col = number_idx * len(GrammarGender) + gender_idx
+                    try:
+                        t[(case, number, gender)] = self.raw_table[(row, col)]
+                    except KeyError:
+                        pass
+        self.adjective_table = t
+
+    def get(self, case, number, gender):
+        return self.adjective_table[(case, number, gender)]
+
 # maps (gender, number) to (file, column)
 def possessive_pronoun(gender, number):
     def f():
@@ -71,14 +147,6 @@ def adjective(hard, gender, number):
     else:
         column = h()
     return fname, column
-
-# case_questions = [
-#     "kdo, co",
-#     "koho, čeho",
-#     "komu, čemu",
-#     "koho, co",
-#     "o kom, o čem",
-#     "s kým, čím"]
 
 def mk_case_dict(*lst):
     if len(lst) == 1:
